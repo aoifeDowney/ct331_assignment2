391b43d0a27a226c5a7ec1f30c818a66590d8e4e
aoifeDowney <aoifestard@gmail.com>
2018-10-31 10:37:03 +0000

    Updated question 1
    I updated how the output is printed out

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,23 +1,18 @@
 #lang racket
 ;A cons pair of two numbers
-(display (string-append "\"" "A cons pair of two numbers" "\""))
 (cons 1 2)
 
 ;A list of 3 numbers, using only the cons function
-(display (string-append "\"" "A list of 3 numbers, using only the cons function" "\""))
 (cons 1 (list 2 3))
 
 ;A list containing a string, a number and a nested list of three numbers
 ;using only the cons function
-(display (string-append "\"" "A list containing a string, a number and a nested list of three numbers using only the cons function" "\""))
 (cons "cons"(cons 1 (cons 2(cons 3 4))))
 
 ;A list containing a string, a number and a nested list of three numbers
 ;using only the list function
-(display (string-append "\"" "A list containing a string, a number and a nested list of three numbers using only the list function" "\""))
 (list "list" 1 (list 2 3 4))
 
 ;A list containing a string, a number and a nested list of three numbers
 ;using only the append function
-(display (string-append "\"" "A list containing a string, a number and a nested list of three numbers using only the append function" "\""))
 (append ' ("append") ' (1) ' ((2 3 4)))
