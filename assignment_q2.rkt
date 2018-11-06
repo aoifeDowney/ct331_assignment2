a3f57cf3ab6f1778dda7f92bc41132712c5c44c6
aoifeDowney <aoifestard@gmail.com>
2018-11-06 11:09:52 +0000

    Updated Q2 & attempted Q.3

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -35,14 +35,33 @@
           ((= (car lst) item) (+ 1 (count_instances (cdr lst) item)))
           (else (count_instances (cdr lst) item))))
 
+;Part E
+
+
 
 ;displaying the answers to the questions
+;Part A
 (display "The ins_beg function:\n")
 (ins_beg 'a '(b c d))
 
+;Part B
 (display "The ins_end function:\n")
 (ins_end '(a b) '(b c d))
 
+;Part C
 (display "The count_top_level function:\n")
 (count_top_level '(1 2 3 (4 5)))
+
+;Part D
+(display "The count_instances function:\n")
+(count_instances '(1 3 3 3 4)3)
+
+;Part E
+(display "The count_instances_tr function:\n")
+
+
+
+
+
+
 
--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -2,16 +2,17 @@
 
 ;A Binary Search Tree
 (define bst '(2 (1 ()()) (3) () ()))
+;(define tree '(((() 3 ()) 8 (() 11 ())) 19 ((() 25 ()) 29 (() 52 ()))))
 
 ;Retruns value of node
 (define (value node)
   (if (null? node) '()
-      (car node)))
+      (cadr node)))
 
 ;Returns the left subtree of node
 (define (left node)
   (if (null? node) '()
-      (cadr node)))
+      (car node)))
 
 ;Returns the right subtree of node
 (define (right node)
@@ -19,15 +20,28 @@
       (caddr node)))
 
 ;Part A
-(define (sortTree tree)
-  (if (empty? tree)
-      empty
-      (append (sortTree (right tree))
-              (cons (value tree)
-                    (sortTree (left tree))))))
+;(define (sort tree)
+ ; (if (null? tree) '()
+  ;    (append (sort (left tree))
+   ;           (list (value tree))
+    ;          (sort (right tree)))))
+
+
+;(define (tree-sort tree)
+ ; (if (null? tree)
+  ;    '()
+   ;   (append (tree-sort (left tree))
+    ;          (list (value tree))
+     ;         (tree-sort (right tree)))))
+
+(define (sort bst);sort left then sort right
+ (begin(cond [(not (empty?(left bst))) (sort (left bst))])
+   (printf "~a " (value bst));
+   (cond [(not (empty?(right bst))) (sort (right bst))])))
+
 
 ;Test
 (display "The tree:\n")
 (left bst)
-(sortTree bst)
+(sort bst)
 
